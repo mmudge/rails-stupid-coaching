b97a2bff28a5a64f47e95bde2c58fc952a7e7866
michael mudge <michaeltmudge@gmail.com>
2018-10-29 13:11:19 +0100

    finished coach exercise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get '/questions', to: 'questions#home'
+  get 'ask', to: 'questions#ask'
+  get 'answer_question', to: 'questions#answer'
 end
+
